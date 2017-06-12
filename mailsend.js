84d2ccf48b6128e6d9c8468ef0054f30e97e54b5
Virunee <viravongv@gmail.com>
2017-06-12 21:43:30 +0100

    Added form validation

--- mailsend.js
+++ mailsend.js
@@ -1,5 +1,10 @@
-$(function() {
-    $(".button").click(function() {
-
+// wait for the DOM to be loaded
+$(document).ready(function() {
+    // bind 'myForm' and provide a simple callback function
+    $('#myForm').ajaxForm(function() {
+        alert("Thanks! We've received your message and will try to get back to you within 24 hours.");
+        $( '#myForm' ).each(function(){
+          this.reset();
+        });
     });
-  });
+});
--- mailsend.php
+++ mailsend.php
@@ -1,15 +1,16 @@
 <?PHP
 $email = $_POST["email"];
+$submittedmessage = $_POST["message"];
 $to = "viravongv@gmail.com";
 $subject = "New Email Address for Mailing List";
 $headers = "From: $email\n";
-$message = "A visitor to your site has sent the following email address to be added to your mailing list.\n
-            Email Address: $email";
+$message = "A visitor to your site has sent the following message to be added to your mailing list.\n
+            Message: $submittedmessage";
 
 $user = "$email";
 $usersubject = "Thank You";
 $userheaders = "From: viravongv@gmail.com\n";
-$usermessage = "Thank you for subscribing to our mailing list.";
+$usermessage = "Thank you for your message. We will get back to you shortly!";
 
 mail($to,$subject,$message,$headers);
 mail($user,$usersubject,$usermessage,$userheaders);
